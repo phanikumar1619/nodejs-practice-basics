f7f24f9bd3f5f1e2406de6a6ac88ceff1808edfc
k.phani_kumar <2200032947@kluniversity.in>
2026-09-11 22:53:13 +0530

    feat: return null from divide when dividing by zero

--- recap/03-math.js
+++ recap/03-math.js
@@ -8,16 +8,18 @@ const subtract = (a ,b) => {
     return a - b;
 }
 
-function multiply(a ,b) {
+const multiply = (a ,b) => {
     return a * b;
 }   
 
 const divide = (a ,b) => {
+    if(b === 0){
+        return null;
+    }
     return a / b;
 }
 
 module.exports = {
-    version,
     add,
     subtract,
     multiply,
