3e99c7e05be81d08e90e2ae50089aea171e0393d
k.phani_kumar <2200032947@kluniversity.in>
2026-09-16 21:45:38 +0530

    feat: finish os module exercise with currentOS object

--- recap/06-os.js
+++ recap/06-os.js
@@ -1,4 +1,4 @@
-const currentos = require("os");
+// const currentos = require("os");
 
 // console.log(`type of os used in the my system: ${currentos.type()}`)
 // console.log(`the version of my system: ${currentos.release()}`)
@@ -9,12 +9,12 @@ const currentos = require("os");
 
 const { totalmem , uptime} = require("os");
 const gb = totalmem()/(1024 * 1024 * 1024);
-console.log(`Total Memory: ${gb.toFixed(1)} GB`)
+console.log(`Total Memory: ${gb.toFixed(1)} GB`);
 
 const days = uptime()/86400;
 const hours = uptime() / (60 * 60);
-console.log(`Uptime: ${days.toFixed(1)} days`)
-console.log(`Uptime: ${hours.toFixed(1)} hours`)
+console.log(`Uptime: ${days.toFixed(1)} days`);
+console.log(`Uptime: ${hours.toFixed(1)} hours`);
 
 const per = (totalmem() - currentos.freemem())/totalmem() * 100;
-console.log(`Memory Usage: ${per.toFixed(1)}%`)
+console.log(`Memory Usage: ${per.toFixed(1)}%`);
