3e2afdb13b3ece41ca9d7186020547bfb5a1e314
k.phani_kumar <2200032947@kluniversity.in>
2026-09-18 20:20:53 +0530

    style: format notes app with standard spacing

--- recap/09-notes.js
+++ recap/09-notes.js
@@ -3,16 +3,16 @@ const fs = require("fs");
 
 const data = process.argv.slice(2);
 
-const filepath = path.join(__dirname , '..' , 'content' ,  'notes.txt');
+const filepath = path.join(__dirname, '..', 'content', 'notes.txt');
 
-if(data.length === 0){
+if (data.length === 0) {
     console.log("Please give some notes.  Example: node 09-notes.js 'my notes'");
-}else{
-    fs.writeFileSync(filepath , `${data.join(' ')}\n` , {flag : 'a'});
-    const text = fs.readFileSync(filepath , 'utf-8');
+} else {
+    fs.writeFileSync(filepath, `${data.join(' ')}\n`, { flag: 'a' });
+    const text = fs.readFileSync(filepath, 'utf-8');
     const lines = text.split('\n');
     const notes = lines.filter((line) => line.trim() !== '');
-    for(let i = 1 ; i <= notes.length ; i++){
-        console.log(`${i}. ${notes[i-1]}`);
+    for (let i = 1; i <= notes.length; i++) {
+        console.log(`${i}. ${notes[i - 1]}`);
     }
-}
+} 
