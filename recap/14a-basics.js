18a3fa2641ad2b1a0aaa5bc7955072393b56b618
k.phani_kumar <2200032947@kluniversity.in>
2026-09-21 22:53:39 +0530

    feat: express basics with routes and 404

---
+++ recap/14a-basics.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.status(200).send(`
+        <h1>Welcome to our home page</h1>
+        <a href = "/about">About</a>
+        <a href = "/contact">Contact</a>
+        <a href = "/hello">Hello</a>`);
+});
+
+app.get('/about', (req, res) => {
+    res.status(200).send(`
+        <h1>About Us</h1>
+        <p>My name is KARUBHUKTHA PHANI KUMAR</p>
+        <a href = "/">back to home</a>`);
+});
+
+app.get('/contact', (req, res) => {
+    res.status(200).send(`
+        <h1>Contact Us</h1>
+        <p>This is the contact page.</p>
+        <p>Contact us at: example@email.com</p>
+        <a href = "/">back to home</a>`);
+});
+
+app.get('/hello', (req, res) => {
+    res.status(200).send('Hello Good Morning user');
+});
+
+app.use((req, res) => {
+    res.status(404).send(`
+        <h1>Oops!</h1>
+        <p>The page you are looking for does not exist.</p>
+        <a href = "/">back to home</a>`);
+});
+
+//question: Move app.listen to the top of the file, above the routes. Restart, visit /about. Does it still work? (It does — think about why: registering routes is instant, and requests only arrive later.)
+// yes working perfectly because the app.listen is just starting the server and registering the routes is instant, so it doesn't matter where you place the app.listen in the code. The routes will still be registered and available for requests.
+
+//question: Move your app.use 404 above app.get('/about'). What happens to /about?
+// the route is giveing 404 error because the app.use 404 is above the app.get('/about') route.
+
+// question: what will happen if we have two app.get('/about', ...) routes with different text. no one will win. and give the error in the brower.
+// app.get('/about', (req, res) => {
+//     res.status(200).send(`
+//         <h1>About Us</h1>
+//         <p>My name is KARUBHUKTHA PAVAN KUMAR</p>
+//         <a href = "/">back to home</a>`);
+// });
+// two app.get('/about', ...) routes with different text. no one will win. and give the error in the brower.
